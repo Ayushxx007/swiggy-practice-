2e7d3e2045b70c1c5bfd0129d97ac4c2a15ec888
“Ayush <ayushkumawat77889@gmail.com>
2025-04-24 05:24:45 +0530

    pracx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React,{Component} from 'react'
 
-const About = () => {
-  return (
-    <div>
-      about
-    </div>
-  )
+
+
+class About extends Component {
+
+  constructor(props){
+    console.log("constructor has been called");
+    super(props);
+    this.state = {
+      count:0
+    }
+
+  }
+
+  incrementCount = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+
+  componentDidMount(){
+    console.log("component has mounted");
+
+  }
+
+  componentDidUpdate(){
+    console.log("component has updated");
+
+  }
+
+  componentWillUnmount(){
+    console.log("component has unmounted");
+  }
+
+
+
+  render(){
+
+    return (
+      <div>
+
+       <div>rendering</div>
+       <button onClick={this.incrementCount}>click</button>
+       <div>{this.state.count}</div>
+
+
+      </div>
+    
+
+    )
+
+  }
+
+
+
+
+
+
+
+
 }
 
-export default About
+export default About;
+
